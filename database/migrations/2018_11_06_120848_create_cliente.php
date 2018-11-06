c81c0dc6041c74145ec0dd66bdeecf87b74ffd0a
amansil3 <andres.mansilla.02@gmail.com>
2018-11-06 09:33:07 -0300

    asdasd

---
+++ database/migrations/2018_11_06_120848_create_cliente.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCliente extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre',50);
+            $table->string('apellido',50);
+            $table->string('direccion',50);
+            $table->integer('telefono',50);
+            $table->integer('dni',50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
